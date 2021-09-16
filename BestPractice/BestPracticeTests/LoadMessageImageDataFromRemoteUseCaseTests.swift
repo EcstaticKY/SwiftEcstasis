699260503e53663e1f5a38ca4fffc1a25401e60f
nameky <suchbalance@qq.com>
2021-09-16 15:09:37 +0800

    `load` requests image data with URL

--- BestPractice/BestPracticeTests/LoadMessageImageDataFromRemoteUseCaseTests.swift
+++ BestPractice/BestPracticeTests/LoadMessageImageDataFromRemoteUseCaseTests.swift
@@ -7,25 +7,43 @@ import XCTest
 import BestPractice
 
 final class RemoteMessageImageDataLoader {
+    private let client: HTTPClient
     
+    init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    func load(from url: URL) {
+        client.get(from: url) { _ in }
+    }
 }
 
 class LoadMessageImageDataFromRemoteUseCaseTests: XCTestCase {
 
     func test_init_doesNotRequestImageData() {
         let client = HTTPClientSpy()
-        let _ = RemoteMessageImageDataLoader()
+        let _ = RemoteMessageImageDataLoader(client: client)
         XCTAssertTrue(client.requestURLs.isEmpty)
     }
     
+    func test_load_requestsImageDataWithURL() {
+        let url = anyURL()
+        let client = HTTPClientSpy()
+        let sut = RemoteMessageImageDataLoader(client: client)
+        sut.load(from: url)
+        XCTAssertEqual(client.requestURLs, [url])
+    }
+    
     // MARK: - Helpers
+    
+    private func anyURL() -> URL { URL(string: "https://any-url.com")! }
     
     private class HTTPClientSpy: HTTPClient {
         
         var requestURLs = [URL]()
         
         func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) {
-            
+            requestURLs.append(url)
         }
     }
 }
