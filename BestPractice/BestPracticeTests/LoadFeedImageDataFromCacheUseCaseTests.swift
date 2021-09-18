06f0704ffaa42f74f45766322364f7813edf301a
nameky <suchbalance@qq.com>
2021-09-18 11:15:27 +0800

    `Load` requests image data with URL

--- BestPractice/BestPracticeTests/LoadFeedImageDataFromCacheUseCaseTests.swift
+++ BestPractice/BestPracticeTests/LoadFeedImageDataFromCacheUseCaseTests.swift
@@ -5,22 +5,57 @@
 
 import XCTest
 
+protocol MessageImageDataStore {
+    func retrieve(with url: URL)
+}
+
 class LocalMessageImageDataLoader {
+    private let store: MessageImageDataStore
     
+    init(store: MessageImageDataStore) {
+        self.store = store
+    }
+    
+    func load(from url: URL) {
+        store.retrieve(with: url)
+    }
 }
 
 class LoadMessageImageDataFromCacheUseCaseTests: XCTestCase {
 
     func test_init_doesNotRequestImageData() {
-        let store = MessageImageDataStoreSpy()
-        let _ = LocalMessageImageDataLoader()
+        let (_, store) = makeSUT()
         
-        XCTAssert(store.loadURLs.isEmpty)
+        XCTAssertTrue(store.loadURLs.isEmpty)
+    }
+    
+    func test_load_requestsImageDataWithURL() {
+        let url = anyURL()
+        let (sut, store) = makeSUT()
+        
+        sut.load(from: url)
+        
+        XCTAssertEqual(store.loadURLs, [url])
     }
     
     // MARK: - Helpers
-    private class MessageImageDataStoreSpy {
+    
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line)
+        -> (sut: LocalMessageImageDataLoader, store: MessageImageDataStoreSpy) {
+        
+        let store = MessageImageDataStoreSpy()
+        let sut = LocalMessageImageDataLoader(store: store)
+        trackForMemoryLeak(store, file: file, line: line)
+        trackForMemoryLeak(sut, file: file, line: line)
+        return (sut, store)
+    }
+    
+    private class MessageImageDataStoreSpy: MessageImageDataStore {
         
         var loadURLs = [URL]()
+        
+        func retrieve(with url: URL) {
+            loadURLs.append(url)
+        }
     }
 }
